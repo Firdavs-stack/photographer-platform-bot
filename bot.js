9df947a5001ee12b002e9360483c56fc13accc52
Usmanov Firdavs <firdavsusmanov418@gmail.com>
2024-11-26 11:45:37 +0500

    link to site changed

--- bot.js
+++ bot.js
@@ -53,7 +53,7 @@ bot.on("message", async (msg) => {
 								{
 									text: "🔍 Открыть поиск фотографов",
 									web_app: {
-										url: "https://thumbnail-infrastructure-ground-ul.trycloudflare.com",
+										url: "https://two2one.uz/",
 									},
 								},
 							],
