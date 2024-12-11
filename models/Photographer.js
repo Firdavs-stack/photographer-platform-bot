c867558d90d5164ebdbb4cdded9038862fcb5861
Usmanov Firdavs <firdavsusmanov418@gmail.com>
2024-12-11 11:47:51 +0500

    hasPastDates flag added to schedule

--- models/Photographer.js
+++ models/Photographer.js
@@ -1,36 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const portfolioItemSchema = new mongoose.Schema({
-    imagePath: String,
-    title: String,
-    category: String,
+	imagePath: String,
+	title: String,
+	category: String,
 });
 
 const photographerSchema = new mongoose.Schema({
-    telegramId: String,
-    phoneNumber: String,
-    telegramUsername: { type: String, required: false }, // Добавляем это поле
-    firstName: String,
-    lastName: String,
-    age: Number,
-    experience: String,
-    favoriteStyles: [String],
-    profilePhoto: String,
-    portfolio: [portfolioItemSchema],
-    status: {
-        type: String,
-        enum: ['novice', 'intermediate', 'pro'], // Добавьте 'pro' в список
-        required: true
-    },
-    hourlyRate: Number,
-    sessionTypes: [String],
-    schedule: [
-        {
-            date: { type: Date, required: true },
-            availableSlots: [String]
-        }
-    ],
-    paymentDetails: { type: String }, // Новое поле для реквизитов
+	telegramId: String,
+	phoneNumber: String,
+	telegramUsername: { type: String, required: false }, // Добавляем это поле
+	firstName: String,
+	lastName: String,
+	age: Number,
+	experience: String,
+	favoriteStyles: [String],
+	profilePhoto: String,
+	portfolio: [portfolioItemSchema],
+	status: {
+		type: String,
+		enum: ["novice", "intermediate", "pro"], // Добавьте 'pro' в список
+		required: true,
+	},
+	hourlyRate: Number,
+	sessionTypes: [String],
+	schedule: [
+		{
+			date: { type: Date, required: true },
+			availableSlots: [String],
+			hasPastDates: Boolean,
+		},
+	],
+	paymentDetails: { type: String }, // Новое поле для реквизитов
 });
 
-module.exports = mongoose.model('Photographer', photographerSchema);
+module.exports = mongoose.model("Photographer", photographerSchema);
