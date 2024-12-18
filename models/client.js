103131a1c0dd0d68b3608025e43ba0b8870f9db3
Usmanov Firdavs <firdavsusmanov418@gmail.com>
2024-12-18 12:30:49 +0500

    testing vip status giving

--- models/client.js
+++ models/client.js
@@ -5,6 +5,19 @@ const clientSchema = new mongoose.Schema({
 	telegramUsername: { type: String, required: false }, // Добавляем это поле
 	name: String,
 	phone: String,
+	photographers: [
+		{
+			photographerId: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Photographer",
+			}, // Ссылка на фотографа
+			status: {
+				type: String,
+				enum: ["regular", "vip"],
+				default: "regular",
+			}, // Статус для каждого фотографа
+		},
+	],
 	referringPhotographerId: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: "Photographer",
