1147d07b0d2aec44a676566063f635fd271c14da
Usmanov Firdavs <firdavsusmanov418@gmail.com>
2024-12-20 17:36:10 +0500

    testing vip staus priviliages

--- controllers/photographerController.js
+++ controllers/photographerController.js
@@ -610,7 +610,7 @@ async function processBookingsByDate(bot, chatId, text, photographer) {
 				buttons.push([
 					{
 						text: "Подтвердить",
-						callback_data: `confirm_booking_photographer;${booking._id.trim()}`,
+						callback_data: `confirm_booking_photographer;${booking._id}`,
 					},
 					{
 						text: "Отменить",
