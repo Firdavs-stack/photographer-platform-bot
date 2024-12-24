4b008f4478875f6c86857dddf3c690093fb7bbb0
Usmanov Firdavs <firdavsusmanov418@gmail.com>
2024-12-24 12:16:43 +0500

    cancel booking functionality

--- bot.js
+++ bot.js
@@ -153,9 +153,9 @@ bot.on("photo", async (msg) => {
 		telegramId: chatId.toString(),
 	});
 
+	bot.sendMessage(chatId, `SOOOSI ${client}`);
 	// Проверка, является ли пользователь клиентом
 	if (client) {
-		bot.sendMessage(chatId, "SOOOSI");
 		// Обработка скриншота оплаты для клиентов
 		if (state && state.state === "awaiting_payment") {
 			const bookingId = state.bookingInfo._id; // Получаем ID бронирования из состояния
